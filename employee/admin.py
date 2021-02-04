397e205b3d6c471e1f448a4f043e5973dc2321ff
Wahaj Ahmad <wahaj@stech.ai>
2021-02-04 16:59:06 +0500

    initial structure

---
+++ employee/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Employee, Department
+from simple_history.admin import SimpleHistoryAdmin
+
+# Register your models here.
+
+
+class EmployeeAdmin(SimpleHistoryAdmin):
+    list_display = ['first_name', 'last_name', 'department']
+    list_filter = ['department']
+
+
+class DepartmentAdmin(SimpleHistoryAdmin):
+    pass
+
+
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Department, DepartmentAdmin)
+
